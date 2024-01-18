58a01681ab98f0ed0264aa676e47b4b4c59dfe52
Sidiq Hersi <hersisidiq@gmail.com>
2024-01-18 15:26:02 +0000

    adds newletter component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react"
 import NavBar from "./components/NavBar.jsx"
 import Hero from "./components/Hero.jsx"
 import Analytics from "./components/Analytics.jsx"
+import Newsletter from "./components/Newsletter.jsx"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <NavBar />
     <Hero />
     <Analytics />
+    <Newsletter />
     </>
   )
 }
